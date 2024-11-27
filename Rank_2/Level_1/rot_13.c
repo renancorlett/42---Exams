5b221b3d68b81a9c851eda60f632505636719cbf
Renan Corlett Silva <rcorlett@c2r4s9.42porto.com>
2024-11-27 15:49:48 +0000

    level1

--- Rank_2/Level_1/rot_13.c
+++ Rank_2/Level_1/rot_13.c
@@ -1,1 +1,57 @@
+/*
+Assignment name  : rot_13
+Expected files   : rot_13.c
+Allowed functions: write
+--------------------------------------------------------------------------------
 
+Write a program that takes a string and displays it, replacing each of its
+letters by the letter 13 spaces ahead in alphabetical order.
+
+'z' becomes 'm' and 'Z' becomes 'M'. Case remains unaffected.
+
+The output will be followed by a newline.
+
+If the number of arguments is not 1, the program displays a newline.
+
+Example:
+
+$>./rot_13 "abc"
+nop
+$>./rot_13 "My horse is Amazing." | cat -e
+Zl ubefr vf Nznmvat.$
+$>./rot_13 "AkjhZ zLKIJz , 23y " | cat -e
+NxwuM mYXVWm , 23l $
+$>./rot_13 | cat -e
+$
+$>
+$>./rot_13 "" | cat -e
+$
+$>
+*/
+
+#include <unistd.h>
+
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+	int i = 0;
+	char c;
+
+	if (ac == 2)
+	{
+		while (av[1][i])
+		{
+			if (av[1][i] >= 'a' && av[1][i] <= 'z')
+				c = (av[1][i] + 13 > 'z') ? av[1][i] - 13 : av[1][i] + 13;
+			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+				c = (av[1][i] + 13 > 'Z') ? av[1][i] - 13 : av[1][i] + 13;
+			else
+				c = av[1][i];
+			write(1, &c, 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
