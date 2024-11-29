dc8d89f866b4810c1998e07580235efa30681d77
Renan Corlett Silva <rcorlett@c2r4s9.42porto.com>
2024-11-29 09:59:56 +0000

    f

--- Rank_2/Level_2/alpha_mirror.c
+++ Rank_2/Level_2/alpha_mirror.c
@@ -1,1 +1,55 @@
+/*
+Assignment name  : alpha_mirror
+Expected files   : alpha_mirror.c
+Allowed functions: write
+--------------------------------------------------------------------------------
 
+Write a program called alpha_mirror that takes a string and displays this string
+after replacing each alphabetical character by the opposite alphabetical
+character, followed by a newline.
+
+'a' becomes 'z', 'Z' becomes 'A'
+'d' becomes 'w', 'M' becomes 'N'
+
+and so on.
+
+Case is not changed.
+
+If the number of arguments is not 1, display only a newline.
+
+Examples:
+
+$>./alpha_mirror "abc"
+zyx
+$>./alpha_mirror "My horse is Amazing." | cat -e
+Nb slihv rh Znzarmt.$
+$>./alpha_mirror | cat -e
+$
+$>
+*/
+
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+	int	i;
+	char	c;
+
+	i = 0;
+	if (ac == 2)
+	{
+		while (av[1][i])
+		{
+			if (av[1][i] >= 'a' && av[1][i] <= 'z')
+				c = 'z' - av[1][i] + 'a';
+			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+				c = 'Z' - av[1][i] + 'A';
+			else
+				c = av[1][i];
+			write(1, &c, 1);
+			i++;
+		}
+	}
+	write (1, "\n", 1);
+	return (0);
+}
