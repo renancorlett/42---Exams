883209e6b1f761bd3a44fb6aa3e37a1af4da65b8
Renan Corlett Silva <renancorlettsilva@Renans-Macbook-Pro.local>
2024-12-02 16:51:09 +0000

    f

--- Rank_2/Level_2/union.c
+++ Rank_2/Level_2/union.c
@@ -1,1 +1,71 @@
+/*
+Assignment name  : union
+Expected files   : union.c
+Allowed functions: write
+--------------------------------------------------------------------------------
 
+Write a program that takes two strings and displays, without doubles, the
+characters that appear in either one of the strings.
+
+The display will be in the order characters appear in the command line, and
+will be followed by a \n.
+
+If the number of arguments is not 2, the program displays \n.
+
+Example:
+
+$>./union zpadinton "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
+zpadintoqefwjy$
+$>./union ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
+df6vewg4thras$
+$>./union "rien" "cette phrase ne cache rien" | cat -e
+rienct phas$
+$>./union | cat -e
+$
+$>
+$>./union "rien" | cat -e
+$
+$>
+*/
+
+#include <unistd.h>
+
+int	check_double(char *str, char c, int pos)
+{
+	int	i;
+
+	i = 0;
+	while (i < pos)
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	j = 0;
+	if (ac == 3)
+	{
+		while (av[1][i])
+		{
+			if (!check_double(av[1], av[1][i], i))
+				write(1, &av[1][i], 1);
+			i++;
+		}
+		while (av[2][j])
+		{
+			if (!check_double(av[2], av[2][i], j) && !check_double(av[1], av[2][j], i))
+				write(1, &av[2][j], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
