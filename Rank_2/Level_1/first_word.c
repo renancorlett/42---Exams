10981b9241229514dca3ef87a10ef6f1afb01c60
Renan Corlett <renancorlett@gmail.com>
2024-12-04 14:53:43 +0000

    Update first_word.c

--- Rank_2/Level_1/first_word.c
+++ Rank_2/Level_1/first_word.c
@@ -37,9 +37,9 @@ int	main(int argc, char *argv[])
 	i = 0;
 	if (argc == 2)
 	{
-		while (argv[1][i] == 32 || (argv[1][i] >= 9 && argv[1][i] <= 13))
+		while (argv[1][i] == ' ' || (argv[1][i] >= '\t' && argv[1][i] <= '\r'))
 			i++;
-		while (argv[1][i] && argv[1][i] != 32 && (argv[1][i] < 9 || argv[1][i] > 13))
+		while (argv[1][i] && argv[1][i] != ' ' && (argv[1][i] < '\t' || argv[1][i] > '\r'))
 		{
 			write(1, &argv[1][i], 1);
 			i++;
