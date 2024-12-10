eee257d95a825de232cf47e89997d2039fc4c329
Renan Corlett <renancorlett@gmail.com>
2024-12-10 12:36:32 +0000

    Update first_word.c

--- Rank_2/Level_1/first_word.c
+++ Rank_2/Level_1/first_word.c
@@ -39,8 +39,10 @@ int	main(int ac, char **av)
 		while (av[1][i] == 32 || av[1][i] == 9)
 			i++;
 		while (av[1][i] && (av[1][i] != 32 && av[1][i] != 9))
+		{	
 			write (1, &av[1][i], 1);
 			i++;
+		}	
 	}
 	write(1, "\n", 1);
 	return (0);
