007d3bf5cd6d4deab4f514239de3d2b1ea4bd4c3
Renan Corlett <renancorlett@gmail.com>
2024-12-11 11:52:11 +0000

    Update rot_13.c

--- Rank_2/Level_1/rot_13.c
+++ Rank_2/Level_1/rot_13.c
@@ -36,19 +36,16 @@ $>
 int	main(int ac, char **av)
 {
 	int i = 0;
-	char c;
 
 	if (ac == 2)
 	{
 		while (av[1][i])
 		{
 			if (av[1][i] >= 'a' && av[1][i] <= 'z')
-				c = (av[1][i] + 13 > 'z') ? av[1][i] - 13 : av[1][i] + 13;
+				av[1][i] = (av[1][i] - 'a' + 13) % 26 + 'a';
 			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				c = (av[1][i] + 13 > 'Z') ? av[1][i] - 13 : av[1][i] + 13;
-			else
-				c = av[1][i];
-			write(1, &c, 1);
+				av[1][i] = (av[1][i] - 'A' + 13) % 26 + 'A';
+			write(1, &av[1][i], 1);
 			i++;
 		}
 	}
