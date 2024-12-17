8bb1c6d9037ed7f740c46ea7ce1fe14118c6d430
Renan Corlett <renancorlett@gmail.com>
2024-12-17 12:24:44 +0000

    Update ft_rrange.c

--- Rank_2/Level_3/ft_rrange.c
+++ Rank_2/Level_3/ft_rrange.c
@@ -20,27 +20,22 @@ Examples:
 
 #include <stdlib.h>
 
-int	*ft_rrange(int start, int end)
+int	*ft_range(int start, int end)
 {
-	int	*range;
 	int	i = 0;
-	int	step = 1;
-	int	n = end - start;
+	int	len = abs(end - start) + 1;
+	int *arr = malloc(sizeof(int) * len);
 
-	if (n < 0)
-		(n *= -1);
-	n++;
-	range = (int *)malloc(sizeof(int) * n);
-	if (range)
+	if(!arr)
+		return (NULL);
+	while (i < len)
 	{
+		arr[i] = end;
 		if (start < end)
-			step = -1;
-		while (i < n)
-		{
-			range[i] = end;
-			end = end + step;
-			i++;
-		}
+			end--;
+		else
+			end++;
+		i++;
 	}
-	return (range);
+	return (arr);
 }
