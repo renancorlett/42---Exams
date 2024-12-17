a7c8ea0107dc13599c8eb480c413e629c5d0ff01
Renan Corlett <renancorlett@gmail.com>
2024-12-17 14:03:32 +0000

    Update print_hex.c

--- Rank_2/Level_3/print_hex.c
+++ Rank_2/Level_3/print_hex.c
@@ -1,5 +1,5 @@
-/*Assignment name  : print_hex
-Expected files   : print_hex.c
+/*Assignment name: print_hex
+Expected files: print_hex.c
 Allowed functions: write
 --------------------------------------------------------------------------------
 
@@ -23,27 +23,28 @@ $*/
 
 int	ft_atoi(char *str)
 {
-	int	n = 0;
+	int	i = 0;
+	int	result = 0;
 
-	while (*str != '\0')
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		n = n * 10;
-		n = n + *str - '0';
-		++str;
+		result = result * 10 + (str[i] - '0');
+		i++;
 	}
-	return (n);
+	return (result);
 }
 void	print_hex(int n)
 {
-	char	hex_digits[] = "0123456789abcdef";
+	char hex[] = "0123456789abcdef";
 
 	if (n >= 16)
 		print_hex(n / 16);
-	write(1, &hex_digits[n % 16], 1);
+	write(1, &hex[n % 16], 1);
 }
 int	main(int ac, char **av)
 {
 	if (ac == 2)
 		print_hex(ft_atoi(av[1]));
-	write(1, "\n", 1);
+	write (1,"\n", 1);
+	return (0);
 }
