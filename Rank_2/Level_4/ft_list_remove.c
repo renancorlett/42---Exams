c0eb69c47abf25885cd52480bdef60cf76fd4687
Renan Corlett <renancorlett@gmail.com>
2024-12-18 10:53:21 +0000

    Update ft_list_remove.c

--- Rank_2/Level_4/ft_list_remove.c
+++ Rank_2/Level_4/ft_list_remove.c
@@ -25,22 +25,20 @@ $>*/
 #include <stdlib.h>
 #include "ft_list.h"
 
-void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	if (begin_list == NULL || *begin_list == NULL)
-		return;
-	
-	t_list *cur = *begin_list;
+   t_list *current;
 
-	if (cmp(cur->data, data_ref) == 0)
-	{
-		*begin_list = cur->next;
-		free(cur);
-		ft_list_remove_if(begin_list, data_ref, cmp);
-	}
-	else
-	{
-		cur = *begin_list;
-		ft_list_remove_if(&cur->next, data_ref, cmp);
-	}
+   if (!begin_list || !*begin_list)
+    return;
+    
+   if (!cmp((*begin_list)->data, data_ref))
+   {
+    current = *begin_list;
+    *begin_list = (*begin_list)->next;
+    free(current);
+    ft_list_remove_if(begin_list, data_ref, cmp);
+   }
+   else
+    ft_list_remove_if(&(*begin_list)->next, data_ref, cmp);
 }
