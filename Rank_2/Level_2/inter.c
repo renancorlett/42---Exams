f3400fba81817d2a1b59215f9fee9aeb9540beeb
Renan Corlett <renancorlett@gmail.com>
2024-12-20 08:29:13 +0000

    Update inter.c

--- Rank_2/Level_2/inter.c
+++ Rank_2/Level_2/inter.c
@@ -1,6 +1,6 @@
 /*
-Assignment name  : inter
-Expected files   : inter.c
+Assignment name: inter
+Expected files: inter.c
 Allowed functions: write
 --------------------------------------------------------------------------------
 
@@ -24,44 +24,36 @@ $>./inter | cat -e
 $
 */
 
-int	check_double(char *str, char c, int pos)
-{
-	int i;
+#include <unistd.h>
 
-	i = 0;
-	while (i < pos)
-	{
-		if (str[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
+void inter(char *s1, char *s2)
+{
+    int seen[256] = {0};
+
+    while (*s2)
+    {
+        seen[(unsigned char)*s2] = 1;
+        s2++;
+    }
+    while (*s1)
+    {
+        if (seen[(unsigned char)*s1])
+        {
+            write(1, s1, 1);
+            seen[(unsigned char)*s1] = 0;
+        }
+        s1++;
+    }
 }
-int	main(int ac, char **av)
-{
-	int	i = 0;
-	int	j;
 
-	if (ac == 3)
-	{
-		while (av[1][i])
-		{
-			if(!check_double(av[1], av[1][i], i))
-			{
-				j = 0;
-				while (av[2][j])
-				{
-					if(av[1][i] == av[2][j])
-					{
-						write(1, &av[1][i], 1);
-						break;
-					}
-					j++;
-				}
-			}
-			i++;
-		}
-	}
-	write (1, "\n", 1);
-	return (0);
+int main(int ac, char **av)
+{
+    if (ac != 3)
+    {
+        write(1, "\n", 1);
+        return 0;
+    }
+    inter(av[1], av[2]);
+    write(1, "\n", 1);
+    return (0);
 }
