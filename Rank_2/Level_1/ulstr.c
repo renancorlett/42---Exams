dfaa7cfc109de9e2ad18168ba316c911abfeecf0
Renan Corlett <renancorlett@gmail.com>
2024-12-24 10:57:46 +0000

    Update ulstr.c

--- Rank_2/Level_1/ulstr.c
+++ Rank_2/Level_1/ulstr.c
@@ -27,20 +27,18 @@ $
 
 int	main(int ac, char **av)
 {
-	int	i;
+	int	i = 0;
 	char	c;
 
-	i = 0;
 	if (ac == 2)
 	{
 		while (av[1][i])
 		{
-			if (av[1][i] >= 'a' && av[1][i] <= 'z')
-				c = av[1][i] - 32;
-			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				c = av[1][i] + 32;
-			else
-				c = av[1][i];
+			c = av[1][i];
+			if (c >= 'a' && c <= 'z')
+				c -= 32;
+			else if (c >= 'A' && c <= 'Z')
+				c += 32;
 			write(1, &c, 1);
 			i++;
 		}
