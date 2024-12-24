ba173a70dfafe51c1267f487252fd6789752b2e9
Renan Corlett <renancorlett@gmail.com>
2024-12-24 10:32:55 +0000

    Update rot_13.c

--- Rank_2/Level_1/rot_13.c
+++ Rank_2/Level_1/rot_13.c
@@ -1,6 +1,6 @@
 /*
-Assignment name  : rot_13
-Expected files   : rot_13.c
+Assignment name: rot_13
+Expected files: rot_13.c
 Allowed functions: write
 --------------------------------------------------------------------------------
 
@@ -31,21 +31,21 @@ $>
 
 #include <unistd.h>
 
-#include <unistd.h>
-
 int	main(int ac, char **av)
 {
-	int i = 0;
+	int	i = 0;
+	char	c;
 
 	if (ac == 2)
 	{
 		while (av[1][i])
 		{
+			c = av[1][i];
 			if (av[1][i] >= 'a' && av[1][i] <= 'z')
-				av[1][i] = (av[1][i] - 'a' + 13) % 26 + 'a';
+				c = (av[1][i] - 'a' + 13) % 26 + 'a';
 			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				av[1][i] = (av[1][i] - 'A' + 13) % 26 + 'A';
-			write(1, &av[1][i], 1);
+				c = (av[1][i] - 'A' + 13) % 26 + 'A';
+			write(1, &c, 1);
 			i++;
 		}
 	}
