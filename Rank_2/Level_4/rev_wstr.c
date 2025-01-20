211cd2f3d2a06c1210c30bfb110559661a6ae002
Renan Corlett <renancorlett@gmail.com>
2025-01-20 11:21:32 +0000

    Update rev_wstr.c

--- Rank_2/Level_4/rev_wstr.c
+++ Rank_2/Level_4/rev_wstr.c
@@ -30,30 +30,30 @@ $>*/
 
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
 	int start;
 	int end;
 	int i = 0;
 	int flag;
 		
-	if(argc == 2)
+	if (ac == 2)
 	{   
-		while(argv[1][i] != '\0')
+		while (av[1][i] != '\0')
 			i++;
-		while(i >= 0)
+		while (i >= 0)
 		{
-			while( argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')
+			while (av[1][i] == '\0' || av[1][i] == ' ' || av[1][i] == '\t')
 				i--;
 			end = i;
-			while(argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+			while(av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
 				i--;
 			start = i + 1;
 			int  flag;
 			flag = start;
 			while(start <= end)
 			{
-				write (1, &argv[1][start],1);
+				write (1, &av[1][start], 1);
 				start++;		
 			}
 			if (flag != 0)
